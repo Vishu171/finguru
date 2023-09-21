40cfc1d983fa0a9d0d8e73da7f43c37f2ff71cda
Vishu171 <100827321+Vishu171@users.noreply.github.com>
2023-09-21 17:03:22 +0530

    update

--- streamlit-buffett-main/FinGuru_app.py
+++ streamlit-buffett-main/FinGuru_app.py
@@ -111,9 +111,9 @@ tab1, tab2, tab3 = st.tabs([
 with st.sidebar:
   
 
-    from PIL import Image
-    image = Image.open('/content/drive/MyDrive/NewSnowflake/streamlit-buffett-main/assets/Jade.png')
-    st.image(image, caption='')
+    #from PIL import Image
+    #image = Image.open('/content/drive/MyDrive/NewSnowflake/streamlit-buffett-main/assets/Jade.png')
+    #st.image(image, caption='')
 
    
 
