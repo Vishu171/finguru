50010d5e1f47159eac18b778117193672df56375
Vishu171 <100827321+Vishu171@users.noreply.github.com>
2023-09-21 16:54:37 +0530

    Update FinGuru_app.py

--- streamlit-buffett-main/FinGuru_app.py
+++ streamlit-buffett-main/FinGuru_app.py
@@ -5,13 +5,7 @@ import streamlit as st
 import altair as alt
 import prompts
 #import tkinter as tk
-import warnings
-from datetime import datetime as dt
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LinearRegression
-warnings.filterwarnings('ignore')
-import streamlit as st
-from snowflake.snowpark import Session
+
 
 
 st.set_page_config(layout="wide")
@@ -107,11 +101,11 @@ def format_func(option):
 
 
 # create tabs
-tab1, tab2, tab3, tab4 = st.tabs([
+tab1, tab2, tab3 = st.tabs([
     "Explore Company Statements ", 
     "Explore Company Status ",
-    "Explore Annual Reports ",
-    "Predict Revenue"]
+    "Explore Annual Reports "
+   ]
           
     )
 
@@ -312,65 +306,6 @@ with tab3:
             except:
                 st.write("Please try to improve your question")
 
-with tab4:
-    st.markdown("""
-    ### Predict Revenue
-
-    FinGuru, our AI expert can help you with predictions on revenue. Consider, Apple.Inc, a multinational technology company as an example. To predict revenue, we typically need data on past revenues, market conditions, sales projections, and other relevant factors. Here we are considering spend on R&D for last 10 years to analyze trends and predict the revenue of the multinational giant.
-
- 
-
-    
-    
-   
-    """)
-    CONNECTION_PARAMETERS = {
-      "account": "od21480.ap-southeast-1",
-      "user": "JADE",
-      "password": "Welcome@123",
-       "database": "FINANCIALS",
-      "schema": "PROD",
-      "warehouse": "COMPUTE_WH",
-     }
-    sql_query='SELECT * FROM FINANCIALS.PROD.FINANCE_PREDICT'
-    session = Session.builder.configs(CONNECTION_PARAMETERS).create()
-    df = session.sql(sql_query).collect()
-    df = pd.DataFrame(df, columns=['DATE','REVENUE','RND'])
-    df["DATE"] = df['DATE'].apply(lambda x: dt.strptime(x, '%b-%y'))
-    ts_sales = df['REVENUE']
-    ts_rnd = df['RND']
-    X_train, X_test, y_train, y_test = train_test_split(ts_rnd, ts_sales, train_size=0.8, random_state=50)
-    lr = LinearRegression()
-    lr.fit(np.array(X_train.values).reshape(-1,1), np.array(y_train.values).reshape(-1,1))
-   
-   
-    str_input=[]
-    str_input = st.text_input(label='Enter the quarterly R&D spend amount in million(s). If you would like to provide values for more than one quarter, please enter the values separated by spaces, and FinGuru will be able to assist you further')
-  
-    
-    #input=', '.join(str_input)
-    str_input = str_input.split()
-   
-    input = [int(item) for item in str_input if item.isdigit()]
-   
-    #if input:
-      #y_pred = lr.predict(np.array(input).reshape(-1,1))
-   
-
-      #y_pred = pd.DataFrame(y_pred)
-      #y_pred.columns = ["Predicted Revenue"]
-      #st.write(y_pred)
-      #nost.write("The predicted Revenue for {} is {}" % (input,str(y_pred)))
-
-    if input:
-       y_pred = lr.predict(np.array(input).reshape(-1,1))
-       y_pred = pd.DataFrame(y_pred)
-       y_pred.columns = ["Predicted Revenue"]
-       #st.write(y_pred)
-       y_pred["Predicted Revenue"] = y_pred["Predicted Revenue"].apply(lambda x: round(x, 2))
-
-       listToStr = ', '.join([str(i) for i in y_pred["Predicted Revenue"].values])
-       st.write("Predicted Revenue is {} million(s).".format(listToStr))
 
     
     
