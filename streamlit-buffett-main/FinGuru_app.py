60efc192cf2ec2b0c6e497a0af73e05a6cd70825
Vishu171 <100827321+Vishu171@users.noreply.github.com>
2023-09-22 14:50:38 +0530

    Update FinGuru_app.py

--- streamlit-buffett-main/FinGuru_app.py
+++ streamlit-buffett-main/FinGuru_app.py
@@ -141,9 +141,7 @@ with tab1:
 
    
     
-    str_input = st.text_input(label='Enter the question:')
-    st.write("str_input:", str_input)
-    st.write(f"Length of str_input: {len(str_input)}")
+    str_input = st.text_input(label=' Enter the question: :question:') 
 
     if len(str_input) > 1:
         #with st.spinner('Looking up your question i'):
