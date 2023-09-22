5c19f4e3c78bd553d325b17699b02968f064945e
Vishu171 <100827321+Vishu171@users.noreply.github.com>
2023-09-22 16:55:10 +0530

    Update create_ddl_embeddings.py

--- streamlit-buffett-main/load/create_ddl_embeddings.py
+++ streamlit-buffett-main/load/create_ddl_embeddings.py
@@ -5,7 +5,7 @@ from langchain.vectorstores import FAISS
 import streamlit as st
 
 # load the ddl file
-loader = TextLoader('load/ddls.sql')
+loader = TextLoader('streamlit-buffett-main/load/ddls.sql')
 data = loader.load()
 
 # split the text
@@ -17,4 +17,4 @@ embeddings = OpenAIEmbeddings(openai_api_key = st.secrets["openai_key"])
 docsearch = FAISS.from_documents(texts, embeddings)
 
 # save the faiss index
-docsearch.save_local("faiss_index")
+docsearch.save_local("streamlit-buffett-main/faiss_index")
