2bb318babae98c751ba3128b5e07fdee922cd53b
Vishu171 <100827321+Vishu171@users.noreply.github.com>
2023-09-22 14:08:11 +0530

    Update FinGuru_app.py

--- streamlit-buffett-main/FinGuru_app.py
+++ streamlit-buffett-main/FinGuru_app.py
@@ -8,21 +8,22 @@ import prompts
 
 
 
+
 st.set_page_config(layout="wide")
 
 # Variables
 sf_db = st.secrets["sf_database"]
 sf_schema = st.secrets["sf_schema"]
-tick_list = {'BRK.A': "Bershire Hathaway(BRK.A)",
-             'AAPL': "Apple(AAPL)",
-             'PG' : "Proctor and Gamble(PG)",
-             'JNJ' : "Johnson and Johnson(JNJ)",
-             'MA' : "Mastercard(MA)",
-             'MCO' : "Moodys Corp(MCO)",
-             'VZ' : "Verizon(VZ)",
-             'KO' : "Kotak(KO)",
-             'AXP' : "American Express(AXP)",
-             'BAC' : "Bank of America(BAC)"}
+tick_list = {'BRK.A': "Bershire Hathaway (BRK.A)",
+             'AAPL': "Apple (AAPL)",
+             'PG' : "Proctor and Gamble (PG)",
+             'JNJ' : "Johnson and Johnson (JNJ)",
+             'MA' : "Mastercard (MA)",
+             'MCO' : "Moodys Corp (MCO)",
+             'VZ' : "Verizon (VZ)",
+             'KO' : "Kotak (KO)",
+             'AXP' : "American Express (AXP)",
+             'BAC' : "Bank of America (BAC)"}
 #tick_list = ['BRK.A','AAPL','PG','JNJ','MA','MCO','VZ','KO','AXP', 'BAC']
 fin_statement_list = ['income_statement','balance_sheet','cash_flow_statement']
 year_cutoff = 20 # year cutoff for financial statement plotting
@@ -77,7 +78,7 @@ def fs_chain(str_input):
     the prompts.fs_chain is used but with caching
     """
     output = prompts.fs_chain(str_input)
-    type(output)
+    #type(output)
     return output
 
     
@@ -108,13 +109,13 @@ tab1, tab2, tab3 = st.tabs([
           
     )
 
-
 with st.sidebar:
   
        from PIL import Image
        image = Image.open('streamlit-buffett-main/assets/Jade.png')
 
        image = st.image("streamlit-buffett-main/assets/Jade.png",width=150)
+
    
 
    
@@ -123,9 +124,10 @@ with tab1:
     st.markdown("""
     # Greetings from FinGuru !! 
     
-    Are you worried about your company's financial stability? Are you wondering what your income statements, balance sheets and cash flow statements are trying to tell you??
+    A warm welcome to all!
     
-    To answer all your questions here I am, FinGuru, your Intelligent Finance Virtual assistant. I am an AI expert. I will read all your Financial statements and provide answers to your queries on them
+    
+   I'm **FinGuru**, your Intelligent Finance Virtual Assistant.As an AI expert, I'm here to assist you with all your financial inquiries. I possess the ability to analyze financial statements from a variety of companies. Whether you have questions about budgeting, investing, or any other financial topic, I'm here to provide you with insightful answers and guidance. Feel free to ask me anything related to finance!
     
     **Here are some guidelines to ask questions to FinGuru**
 
@@ -139,28 +141,31 @@ with tab1:
 
    
     
-    str_input = st.text_input(label=' Enter the question: :question:')  
+    str_input = st.text_input(label='Enter the question:')
+    st.write("str_input:", str_input)
+    st.write(f"Length of str_input: {len(str_input)}")
 
     if len(str_input) > 1:
+        st.write(str_input)
         #with st.spinner('Looking up your question i'):
-            try:
-                output = fs_chain(str_input)
+            #try:
+                #output = fs_chain(str_input)
                 #st.write(output)
-                try:
+                #try:
                     # if the output doesn't work we will try one additional attempt to fix it
-                    query_result = sf_query(output['result'])
+                    #query_result = sf_query(output['result'])
                    
-                    if len(query_result) > 1:
-                        st.write(query_result)
+                    #if len(query_result) > 1:
+                        #st.write(query_result)
                         #st.write(output)
-                except:
-                    st.write("The first attempt didn't pull what you were needing. Trying again...")
-                    output = fs_chain(f'You need to fix the code but ONLY produce SQL code output. If the question is complex, consider using one or more CTE. Examine the DDL statements and answer this question: {output}')
-                    st.write(sf_query(output['result']))
+                #except:
+                    #st.write("The first attempt didn't pull what you were needing. Trying again...")
+                    #output = fs_chain(f'You need to fix the code but ONLY produce SQL code output. If the question is complex, consider using one or more CTE. Examine the DDL statements and answer this question: {output}')
+                     #st.write(sf_query(output['result']))
                     #st.write(output)
-            except:
-                st.write("Please try to improve your question. Note this tab is for financial statement questions. Use Tab 3 to ask from shareholder letters. Also, only a handful of companies are available, which you can see on the side bar.")
-                st.write(f"Final errored query used:")
+             #except:
+                 #st.write("Please try to improve your question. Note this tab is for financial statement questions. Use Tab 3 to ask from shareholder letters. Also, only a handful of companies are available, which you can see on the side bar.")
+                 #st.write(f"Final errored query used:")
                 #sst.write(output)
     
 
@@ -168,10 +173,10 @@ with tab1:
 with tab2: 
     st.markdown("""
     
-
+    Are you curious to know how other companies are faring? Interested in tracking their financial metrics like Net income, cash flow, profit margin, etc. Eager to read their income statement, balance sheet and profit and loss statements!!
     
     
-    ### FinGuru is here you help you with the Exploration
+    FinGuru is here you help you with the Exploration.Simply select the company from the drop down and get started !!
    
     
     """)
@@ -240,9 +245,8 @@ with tab3:
     st.markdown("""
   
      Your CEO has released the annual letter to the shareholders. It is an important document with key information, performance upddates and company's strategy to shareholders
-
+     
     It is the much awatied document but you are hesitant to read and understand the company's Performance, Strategic outlook, Operational highlights, Market and Industry Analysis and Financial information
-
     ### Call out to FinGuru !!!
     
     
@@ -250,13 +254,12 @@ with tab3:
     )
     
     # Create a text input to edit the selected question
-    #query = st.text_input(label='Enter the question:')
-    query = st.text_input(label=':question: Enter the question: ') 
+    query = st.text_input(label=' Enter the question:')
+   
     #query = st.text_input(label=f'✉️ Enter the question: ')
     # Display the selected question and the edited question
+    #query = st.text_input(label='Enter the question:')
     
-    #st.write('Enter the question:', query)
-    #query = st.text_input("What would you like to ask Warren Buffett?")
     if len(query)>1:
         #with st.spinner('Looking through lots of Shareholder letters now...'):
             
@@ -269,6 +272,11 @@ with tab3:
                 #st.write(result['source_documents'])
             except:
                 st.write("Please try to improve your question")
+
+
+    
+   
+
 
    
 
