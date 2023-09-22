f16d2ddca87f32c3c81d8c9f8e2720f43f8f0626
Vishu171 <100827321+Vishu171@users.noreply.github.com>
2023-09-22 14:16:48 +0530

    Update FinGuru_app.py

--- streamlit-buffett-main/FinGuru_app.py
+++ streamlit-buffett-main/FinGuru_app.py
@@ -157,16 +157,16 @@ with tab1:
                    
                     if len(query_result) > 1:
                         st.write(query_result)
-                        st.write(output)
+                        #st.write(output)
                 except:
                     st.write("The first attempt didn't pull what you were needing. Trying again...")
                     output = fs_chain(f'You need to fix the code but ONLY produce SQL code output. If the question is complex, consider using one or more CTE. Examine the DDL statements and answer this question: {output}')
                     st.write(sf_query(output['result']))
-                    st.write(output)
+                    #st.write(output)
              except:
                  st.write("Please try to improve your question. Note this tab is for financial statement questions. Use Tab 3 to ask from shareholder letters. Also, only a handful of companies are available, which you can see on the side bar.")
                  st.write(f"Final errored query used:")
-                 st.write(output)
+                 #st.write(output)
     
 
 
