0d7492be0fa7a35f829c590276d7af935192e2d8
Vishu171 <100827321+Vishu171@users.noreply.github.com>
2023-09-22 11:56:03 +0530

    Update FinGuru_app.py

--- streamlit-buffett-main/FinGuru_app.py
+++ streamlit-buffett-main/FinGuru_app.py
@@ -151,7 +151,7 @@ with tab1:
                 st.write(f"Inside if Length of str_input: {len(str_input)}")
                 output = fs_chain(str_input)
                 st.write("The string is {}".format(str_input)
-                st.write(output)
+                #st.write(output)
                 try:
                     # if the output doesn't work we will try one additional attempt to fix it
                     query_result = sf_query(output['result'])
