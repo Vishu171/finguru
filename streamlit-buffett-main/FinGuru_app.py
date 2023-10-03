32c8ea469a6b7c3fb9362e093986b0615a11da68
Vishu171 <100827321+Vishu171@users.noreply.github.com>
2023-10-03 18:00:56 +0530

    Update FinGuru_app.py

--- streamlit-buffett-main/FinGuru_app.py
+++ streamlit-buffett-main/FinGuru_app.py
@@ -1,4 +1,3 @@
-
 import snowflake.connector
 import numpy as np
 import pandas as pd
@@ -6,19 +5,14 @@ import streamlit as st
 import altair as alt
 import prompts
 from tabulate import tabulate
-
+from PIL import Image
+from streamlit_option_menu import option_menu
 
 st.set_page_config(layout="wide")
-
-
-
-
 
 # Variables
 sf_db = st.secrets["sf_database"]
 sf_schema = st.secrets["sf_schema"]
-username=st.secrets["streamlit_username"]
-password=st.secrets["streamlit_password"]
 tick_list = {'BRK.A': "Bershire Hathaway(BRK.A)",
              'AAPL': "Apple(AAPL)",
              'PG' : "Proctor and Gamble(PG)",
@@ -32,11 +26,6 @@ tick_list = {'BRK.A': "Bershire Hathaway(BRK.A)",
 #tick_list = ['BRK.A','AAPL','PG','JNJ','MA','MCO','VZ','KO','AXP', 'BAC']
 fin_statement_list = ['income_statement','balance_sheet','cash_flow_statement']
 year_cutoff = 20 # year cutoff for financial statement plotting
-
-
-
-
-
 
 # establish snowpark connection
 conn = st.experimental_connection("snowpark")
@@ -86,8 +75,6 @@ def fs_chain(str_input):
     type(output)
     return output
 
-    
-
 # adding this to test out caching
 st.cache_data(ttl=86400)
 def sf_query(str_input):
@@ -98,25 +85,17 @@ def sf_query(str_input):
     
     return data
 
-
-
 def format_func(option):
     return tick_list[option]
 
-
-
-
-
-
 def creds_entered():
     if len(st.session_state["user1"])>0 and len(st.session_state["passwd"])>0:
-          if  st.session_state["user1"].strip() != username or st.session_state["passwd"].strip() != password: 
+          if  st.session_state["user1"].strip() != "admin" or st.session_state["passwd"].strip() != "admin@123": 
               st.session_state["authenticated"] = False
               st.error("Invalid Username/Password ")
 
-          elif st.session_state["user1"].strip() == username and st.session_state["passwd"].strip() == password:
+          elif st.session_state["user1"].strip() == "admin" and st.session_state["passwd"].strip() == "admin@123":
               st.session_state["authenticated"] = True
-
 
 def authenticate_user():
       if "authenticated" not in st.session_state:
@@ -137,62 +116,15 @@ def authenticate_user():
                   return False
 
 if authenticate_user():
-    # create tabs
-    tab1,  tab3 = st.tabs([
-        "Explore Company Statements ", 
-        
-        "Explore Annual Reports "
-        ]
-              
-        )
-    css = '''
-      <style>
-          .stTabs [data-baseweb="tab-highlight"] {
-              background-color:#00008B;
-          }
-      </style>
-      '''
 
-
-    st.markdown(css, unsafe_allow_html=True)
-
-    st.markdown("""
-      <style>
-
-        .stTabs [data-baseweb="tab-list"] {
-          gap: 5px;
-          }
-
-        .stTabs [data-baseweb="tab"] {
-          height: 15px;
-              white-space: pre-wrap;
-          width: 200px ;
-          background-color: none;
-          color: #00008B;
-          border-radius: 4px 4px 0px 0px;
-          gap: 1px;
-          padding-top: 20px;
-          padding-bottom: 20px;
-          }
-
-        .stTabs [aria-selected="true"] {
-            background-color: none;
-            color: #00008B;
-        }
-
-      </style>""", unsafe_allow_html=True)
     with st.sidebar:
       
-
-        from PIL import Image
-        image = Image.open('streamlit-buffett-main/assets/Newlogo.png')
-        image = st.image("streamlit-buffett-main/assets/Newlogo.png",width=250)
-        
-
-
-      
-    #str_input = st.text_input(label=' Enter the question:') 
-    with tab1:
+      image = Image.open('/content/drive/MyDrive/NewSnowflake/streamlit-buffett-main/assets/F-GeniusJade.png')
+      st.image(image, caption='')
+      selected = option_menu("Main Menu", ["Explore Company Statements", 'Explore Annual Report'], 
+        icons=['house', 'gear'], menu_icon="cast", default_index=1)
+    if selected =='Explore Company Statements':
+        str_input = st.chat_input("Enter your question:")
         st.markdown("""
         I am  Finance Assistant of your company. I possess the ability to extract information from your company's financial statements like balance sheet, income statements, etc spanning across 2003 to 2022. Please ask me questions and I will try my level best to provide accurate responses.
           
@@ -204,122 +136,86 @@ if authenticate_user():
         
         
         """)
-
-      
         
-        str_input = st.text_input(label=' Enter the question:') 
-        if "messages" not in st.session_state:
+        if "messages" not in st.session_state.keys():
               st.session_state.messages = []
 
         for message in st.session_state.messages:
               with st.chat_message(message["role"]):
-                   
                   st.markdown(message["content"], unsafe_allow_html = True)
         
         if prompt := str_input:
-             st.chat_message("user").markdown(prompt, unsafe_allow_html = True)
+            st.chat_message("user").markdown(prompt, unsafe_allow_html = True)
               # Add user message to chat history
-             st.session_state.messages.append({"role": "user", "content": prompt})
+            st.session_state.messages.append({"role": "user", "content": prompt})
 
-        if len(str_input) > 1:
-            #with st.spinner('Looking up your question i'):
+            try:
+                output = fs_chain(str_input)
                 try:
-                    output = fs_chain(str_input)
-                    #st.write(output)
-                    try:
-                    # if the output doesn't work we will try one additional attempt to fix it
-                        query_result = sf_query(output['result'])
-                   
-                        if len(query_result) >= 1:
-                          #st.write(query_result)
-                          with st.chat_message("assistant"):
-                            st.markdown(tabulate(query_result, tablefmt="html"), unsafe_allow_html = True) 
-                        #st.write(output)
-                          st.session_state.messages.append({"role": "assistant", "content": tabulate(query_result, tablefmt="html")})
-                    except:
-                        
-                        st.session_state.messages.append({"role": "assistant", "content": "The first attempt didn't pull what you were needing. Trying again..."})
-                        output = fs_chain(f'You need to fix the code but ONLY produce SQL code output. If the question is complex, consider using one or more CTE. Examine the DDL statements and answer this question: {output}')
-                        st.write(sf_query(output['result']))
-                        #st.write(output)
-
-                    #result_2 = prompts.letter_chain(str_input)
-                    #st.write('PineCone:', result_2['result'])  
-                    #output = fs_chain(str_input)
-                    #query_result = sf_query(output['result'])
-                      #st.write(query_result)
-                    #df_2 = pd.DataFrame(query_result)
-                    #df_str_2 = df_2.to_string(index=True)
-                    #str_input_2 = str_input + ' '+ df_str_2
-                      #st.write(str_input_2)
-                    #result_3 = prompts.letter_chain(str_input_2)
-                    #st.write('Result after sending the question and data:', result_3['result'])
-                    
-                     
-
-                  
-                    #result_2 = prompts.letter_chain(df_str_2)
-                    #st.write('Summary:', result_2['result'])  
-                    #result_4 = prompts.letter_chain(str_input)
-                    #st.write('Result after sending question:', result_4['result'])   
+                # if the output doesn't work we will try one additional attempt to fix it
+                    query_result = sf_query(output['result'])
+                
+                    if len(query_result) >= 1:
+                      with st.chat_message("assistant"):
+                        df_2 = pd.DataFrame(query_result)
+                        df_2.columns = df_2.columns.str.replace('_', ' ')
+                        headers = df_2.columns
+                        st.markdown(tabulate(df_2, tablefmt="html",headers=headers,showindex=False), unsafe_allow_html = True) 
+                      st.session_state.messages.append({"role": "assistant", "content": tabulate(df_2, tablefmt="html",headers=headers,showindex=False)})
                 except:
-                    st.session_state.messages.append({"role": "assistant", "content": "Please try to improve your question. Note this tab is for financial statement questions. Use Tab 2 to ask from Annual Reports ."})
-                    st.session_state.messages.append({"role": "assistant", "content": f"Final errored query used:"})
-                    #sst.write(output)
-        
-
-
-   
-    with tab3:
+                    
+                    st.session_state.messages.append({"role": "assistant", "content": "The first attempt didn't pull what you were needing. Trying again..."})
+                    output = fs_chain(f'You need to fix the code but ONLY produce SQL code output. If the question is complex, consider using one or more CTE. Examine the DDL statements and answer this question: {output}')
+                    st.write(sf_query(output['result']))
+                    
+            except:
+                st.session_state.messages.append({"role": "assistant", "content": "Please try to improve your question. Note this tab is for financial statement questions. Use Tab 2 to ask from Annual Reports ."})
+                st.session_state.messages.append({"role": "assistant", "content": f"Final errored query used:"})
+                #sst.write(output)
+    else:
+        query = st.chat_input("Enter your question:")
         st.markdown("""
-      
+
         I am capable of reviewing the annual reports from 2018 to 2022. Please ask me questions and I will try my level best to provide accurate responses
-               
-         **Some Sample Questions:**
+              
+        **Some Sample Questions:**
       
-         - How many shareholders does JGSDL have?
-         - What are the risks JGSDL is facing?
+        - How many shareholders does JGSDL have?
+        - What are the risks JGSDL is facing?
         
         
-        """
-        )
+        
+        """)
         
         # Create a text input to edit the selected question
-        #query = st.text_input(label='Enter the question:')
-        query = st.text_input(label='Enter the question: ')
-        if "messagestab2" not in st.session_state:
-              st.session_state.messagestab2 = []
+        #query = st.chat_input('Enter the question:')
+        if "messages_1" not in st.session_state.keys():
+              st.session_state.messages_1 = []
 
-        for message in st.session_state.messagestab2:
+        for message in st.session_state.messages_1:
               with st.chat_message(message["role"]):
-                   
+                  
                   st.markdown(message["content"], unsafe_allow_html = True)
-        
-        if prompt := query:
-             st.chat_message("user").markdown(prompt, unsafe_allow_html = True)
+        #query = st.chat_input('Enter the question:')
+        if prompt1 := query:
+            st.chat_message("user").markdown(prompt1, unsafe_allow_html = True)
               # Add user message to chat history
-             st.session_state.messagestab2.append({"role": "user", "content": prompt})
-        #query = st.text_input(label=f'✉️ Enter the question: ')
-        # Display the selected question and the edited question
-        
-        #st.write('Enter the question:', query)
-        #query = st.text_input("What would you like to ask Warren Buffett?")
-        if len(query)>1:
-            #with st.spinner('Looking through lots of Shareholder letters now...'):
+            st.session_state.messages_1.append({"role": "user", "content": prompt1})
+            #query = st.text_input(label=f'✉️ Enter the question: ')
+            # Display the selected question and the edited question
+            
+            #st.write('Enter the question:', query)
+            #query = st.text_input("What would you like to ask Warren Buffett?")
+            
+            try:
+                with st.chat_message("assistant"):
                 
-                try:
-                    #st.caption(":blue[FinGuru's response:]")
-                    #st.write(prompts.letter_qa(query))
-                     with st.chat_message("assistant"):
-                    
-                      result = prompts.letter_chain(query)
-                      st.write(result['result'])
-                    #st.caption(":blue[Source Documents Used] :📄:")
-                    #st.write(result['source_documents'])
-                except:
-                    st.write("Please try to improve your question")
-
-
-      
+                  result = prompts.letter_chain(query)
+                  st.write(result['result'])
+                  st.session_state.messages_1.append({"role": "assistant", "content":result['result'] } )
+                #st.caption(":blue[Source Documents Used] :📄:")
+                #st.write(result['source_documents'])
+            except:
+                st.write("Please try to improve your question")
 
 
