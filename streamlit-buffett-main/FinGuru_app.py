7878b8867e948b2aa2fe9d6bcecc2f2262dd2c67
Vishu171 <100827321+Vishu171@users.noreply.github.com>
2023-12-19 14:24:16 +0530

    Update FinGuru_app.py

--- streamlit-buffett-main/FinGuru_app.py
+++ streamlit-buffett-main/FinGuru_app.py
@@ -94,7 +94,7 @@ if authenticate_user():
       icons=['database', 'filetype-pdf'],  
       default_index=0,
       styles={"container":{"font-family": "Garamond"},
-        "nav-link": {"font-size": "20px", "text-align": "left", "margin":"0px", "--hover-color": "grey"}})
+        "nav-link": {"font-family": "Source Sans Pro"},"font-size": "12px", "text-align": "left", "margin":"0px", "--hover-color": "grey"})
     if selected =='Finance Data':
         str_input = st.chat_input("Enter your question:")
         st.markdown("""
